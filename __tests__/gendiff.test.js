6599039a49af4f09bd8f2bc7e6b6b5b63d96d8bb
Batemir <vadikkarcev666@gmail.com>
2018-04-05 16:51:49 +0300

    test fix

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,13 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-const beforeJson = '__tests__/__fixtures__/before.json';
-const afterJson = '__tests__/__fixtures__/after.json';
-const expected = fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8');
-test('gendiff flat json', () => {
-  const result = gendiff(beforeJson, afterJson);
-  expect(result).toEqual(expected);
+describe('flat gendiff', () => {
+  const beforeJson = '__tests__/__fixtures__/before.json';
+  const afterJson = '__tests__/__fixtures__/after.json';
+  const expected = fs.readFileSync('__tests__/__fixtures__/expected.txt', 'utf-8');
+  test('gendiff flat json', () => {
+    const result = gendiff(beforeJson, afterJson);
+    expect(result).toEqual(expected);
+  });
 });
+
